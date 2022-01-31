2b894576caf5088a43b54473365cd089aafff61e
Li Yang <1175529741@qq.com>
2022-02-01 01:19:44 +0800

    123

--- download_Platform.sh
+++ download_Platform.sh
@@ -1,28 +1,76 @@
 #!/bin/bash
-if [ -d Platform ]; then
-    rm -rf Platform
-fi
+function copy_tmplate_file()
+{
+    file_name="${1}"
+    file_name=${file_name#${template_path}\/}
+    if [ ! -e ${root}/${file_name} ];then
+        cp -rf ${template_path}/${file_name} ${root}/${file_name}
+    fi
+}
 
-git clone git@github.com:LeeYoungCN/Platform.git
+function copy_all_file()
+{
+    for f in $(ls -a $1); do
+        if [ ${f} == "." -o ${f} == ".." ]; then
+            continue
+        fi
+        if [ -d ${f} ]; then
+            copy_all_file "${1}/${f}"
+        else
+            copy_tmplate_file "${1}/${f}"
+        fi
+    done
+}
 
+function backup_file()
+{
+    file="${1}"
+    echo ${file}
+    if [ -d ${file} ]; then
+        mv -f "${file}" "${file}_backup"
+    fi
+}
+
+function reset_from_backup()
+{
+    file="${1}"
+    if [ -d "${file}_backup" ]; then
+        mv -f "${file}_backup" "${file}"
+    fi
+}
+
+function delete_backup()
+{
+    file="${1}"
+    if [ -d "${file}_backup" ]; then
+        rm -rf "${file}_backup"
+    fi
+}
+
+function git_clone_repository()
+{
+    ssh_path="${1}"
+    repository=${ssh_path#*/}
+    repository=${repository%*.git}
+    backup_file "${repository}"
+    git clone "${ssh_path}"
+    if [ $? -ne 0 ]; then
+        reset_from_backup "${repository}"
+        exit
+    fi
+    delete_backup "${repository}"
+}
+
+git_clone_repository "git@github.com:LeeYoungCN/Platform.git"
 root=$(pwd)
 platform_path=$(cd Platform;pwd)
+template_path="${platform_path}/template"
 cd ${platform_path}/build
 ./init_env.sh
 cd ${root}
-
-function copy_tmplate_file()
-{
-    if [ ! -e ${root}/${1} ];then
-        cp -rf ${platform_path}/template/${1} ${root}/${1}
-    fi
-}
 
 if [ ! -d ${root}/.vscode ]; then
     mkdir -p ${root}/.vscode
 fi
 
-copy_tmplate_file ".vscode/tasks.json"
-copy_tmplate_file "CMakeLists.txt"
-copy_tmplate_file "download_Platform.sh"
-copy_tmplate_file ".gitignore"
+copy_all_file ${template_path}
